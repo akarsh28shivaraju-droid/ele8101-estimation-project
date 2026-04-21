a33680987c92216b39e9d826a3a19d259aae94ed
Akarsh Shivarju <akarsh28shivaraju@gmail.com>
2026-04-21 20:20:53 +0100

    Update measurement_1.m

--- src/measurement_1.m
+++ src/measurement_1.m
@@ -1,4 +1,4 @@
-function y = measurement_step1(x, C)
+function y = measurement_1(x, C)
     % Range measurements from vehicle position to each beacon
     p = x(1);
 
@@ -8,6 +8,6 @@ function y = measurement_step1(x, C)
     for i = 1:nB
         true_range = abs(p - C.beacons(i));
         noise = C.sigma_range * randn;
-        y(i) = true_range + noise;
+        y(i) = max(true_range + noise, 0);
     end
 end
