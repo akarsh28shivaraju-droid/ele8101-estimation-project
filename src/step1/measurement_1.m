06849265affbe6b0fac65c37e4bab57b27f82b42
manojmanny17-crypto <manojmanny17@gmail.com>
2026-04-25 15:45:51 +0100

    Creating measurement for path

---
+++ src/step1/measurement_1.m
@@ -0,0 +1,17 @@
+function y = measurement_1(x, C)
+
+p = x(1);
+
+nB = length(C.beacons);
+y = zeros(nB, 1);
+
+for i = 1:nB
+
+    true_range = abs(p - C.beacons(i));
+    noise = C.sigma_range * randn;
+
+    y(i) = max(true_range + noise, 0);
+
+end
+
+end
