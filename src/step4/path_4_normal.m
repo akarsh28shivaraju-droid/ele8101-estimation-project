960d2ca69eb4556c0a89504a02060a0dbfcc569c
Akarsh Shivarju <akarsh28shivaraju@gmail.com>
2026-04-25 16:47:32 +0100

    Create path_4_normal.m

---
+++ src/step4/path_4_normal.m
@@ -0,0 +1,11 @@
+function [centre, normal] = path_4_normal(s, C)
+    ds = 0.01;
+    centre = path_4(s, C);
+    p1 = path_4(s - ds, C);
+    p2 = path_4(s + ds, C);
+
+    tangent = p2 - p1;
+    tangent = tangent / norm(tangent);
+
+    normal = [-tangent(2); tangent(1)];
+end
