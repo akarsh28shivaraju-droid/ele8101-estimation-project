8eb8af3307a931c44ee4a60cd4b3c27b7653f951
Akarsh Shivarju <akarsh28shivaraju@gmail.com>
2026-04-28 00:08:36 +0100

    Update path_4_with_bounds.m

--- src/step4/path_4_with_bounds.m
+++ src/step4/path_4_with_bounds.m
@@ -1,5 +1,6 @@
 function [centre, upper, lower] = path_4_with_bounds(s, C)
     [centre, normal] = path_4_normal(s, C);
+
     half_width = C.road_width / 2;
 
     upper = centre + half_width * normal;
